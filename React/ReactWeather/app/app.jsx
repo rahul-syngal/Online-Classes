1ca920c7b18c3a774e55bf5498245cb4b1c42512
Rahul Syngal <rahulsyngal@Rahuls-Computer.fios-router.home>
2016-11-29 23:14:33 -0500

    Add weather app

---
+++ React/ReactWeather/app/app.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={}></Route>
+  </Router>,
+  document.getElementById('app')
+);
